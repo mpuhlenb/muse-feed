c3c8d43defc3fc2cfb5b6bfa5c938af78a71f897
mpuhlenb <mpuhlenb@yahoo.com>
2023-11-08 13:37:32 +0100

    Add Rijks Museum decodable models Add Rijks Museum Api Service, get collection items.

---
+++ musefeed/musefeed/Network/API Models/Rijks API Models/RijksArtObjects.swift
@@ -0,0 +1,26 @@
+//
+//  RijksArtObjects.swift
+//  musefeed
+//
+//  Created by Morris Uhlenbrauck on 11/8/23.
+//
+
+import Foundation
+
+struct RijksArtObjects: Decodable {
+    let artObjects: [RijksArtObject]
+}
+
+struct RijksArtObject: Decodable {
+    let id: String
+    let title: String
+    let hasImage: Bool
+    let webImage: RijksWebImage
+}
+
+struct RijksWebImage: Decodable {
+    let guid: String
+    let width: Int
+    let height: Int
+    let url: String
+}
