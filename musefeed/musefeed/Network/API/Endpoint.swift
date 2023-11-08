af3310668bf37d773ab3dcd34ac571df80bbecc7
mpuhlenb <mpuhlenb@yahoo.com>
2023-11-08 12:06:53 +0100

    update Endpoint protocol

--- musefeed/musefeed/Network/API/Endpoint.swift
+++ musefeed/musefeed/Network/API/Endpoint.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 protocol Endpoint {
-    var baseUrl: String { get }
+    var host: String { get }
+    var scheme: String { get }
+    var path: String { get }
+    var method: String { get }
+    var parameters: [String: String] { get }
+}
+
+extension Endpoint {
+    // All APIs will use secure htttp requests
+    var scheme: String {
+        return "https"
+    }
+    
+    // Musuem open APIs use GET requests
+    var method: String {
+        return "GET"
+    }
 }
