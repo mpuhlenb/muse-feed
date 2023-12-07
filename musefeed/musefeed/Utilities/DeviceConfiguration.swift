a9b0b2541cf0eb1d4f5363e9ff82798c2e9fce05
mpuhlenb <mpuhlenb@yahoo.com>
2023-12-07 12:10:15 +0100

    Fixup collection header and cell sizing add device configuration properties do not allow user interaction when section refreshing

---
+++ musefeed/musefeed/Utilities/DeviceConfiguration.swift
@@ -0,0 +1,19 @@
+//
+//  DeviceConfiguration.swift
+//  musefeed
+//
+//  Created by Morris Uhlenbrauck on 12/7/23.
+//
+
+import Foundation
+import UIKit
+
+struct DeviceConfiguration {
+    static var isPad: Bool {
+        return UIDevice.current.userInterfaceIdiom == .pad
+    }
+    
+    static var isLandscape: Bool {
+        return UIDevice.current.orientation.isLandscape
+    }
+}
