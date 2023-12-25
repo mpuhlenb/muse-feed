4c4f5ee7ac57b0677303b11eb15a38a5b2526472
mpuhlenb <mpuhlenb@yahoo.com>
2023-12-25 13:00:42 +0100

    Add welcome label

--- musefeed/musefeed/ViewControllers/GetStartedViewController.swift
+++ musefeed/musefeed/ViewControllers/GetStartedViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 class GetStartedViewController: UIViewController, Storyboarded {
 
     weak var coordinator: MainCoordinator?
+    @IBOutlet var welcomeLabel: UILabel!
     
     @IBOutlet var getStartedButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        welcomeLabel.text = "Welcome to Muse Feed! Are you ready to be inspired by images from museums across the globe? You never know what you might discover!"
+        welcomeLabel.textColor = .foreground
 
     }
 
